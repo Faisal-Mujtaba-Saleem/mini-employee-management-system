a3ae525abc2c7513840f93236491529483d87012
Faisal-Mujtaba-Saleem <faisalmujtaba2005@gmail.com~>
2025-08-11 22:59:47 -0700

    Added serverless-http to express app to make it able to run on vercel.

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
 require("dotenv").config({ path: ".env.local" });
 
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const employeeRoutes = require("./routes/employee.routes");
 const connectDB = require("./config/db.config");
+const ServerlessHttp = require("serverless-http");
 
 // Connect to DB
 connectDB();
@@ -18,7 +19,11 @@ app.use(cors());
 // Routes
 app.use("/api/employee", employeeRoutes);
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+// Start server for traditonal (server-based) API
+if (process.env.NODE_ENV === "development") {
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+} else {
+  module.exports.handler = ServerlessHttp(app);
+}
