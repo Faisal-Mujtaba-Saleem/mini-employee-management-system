5352876304222d4d337775489e72c7c3c972c4bb
Faisal-Mujtaba-Saleem <faisalmujtaba2005@gmail.com~>
2025-08-12 02:18:59 -0700

    Edit the dest. in vercel.json

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,13 @@
 require("dotenv").config({ path: ".env.local" });
 
 const express = require("express");
-const cors = require("cors");
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const employeeRoutes = require("./routes/employee.routes");
 const connectDB = require("./config/db.config");
-const ServerlessHttp = require("serverless-http");
 
 // Connect to DB
 connectDB();
@@ -19,11 +18,9 @@ app.use(cors());
 // Routes
 app.use("/api/employee", employeeRoutes);
 
-// Start server for traditonal (server-based) API
-if (process.env.NODE_ENV === "development") {
-  app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-  });
-} else {
-  module.exports.handler = ServerlessHttp(app);
-}
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+module.exports = app;
